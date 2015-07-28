ab84696ce97fa610406eb8c39c6317ed15a5fdde
wirover <asherman1024@gmail.com>
2015-07-28 15:45:22 -0500

    Fixed incorrect jrpc version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
   name = "dblog",
-  version = "0.3",
+  version = "0.3.1",
   description = "A Python logging library that supports databases",
   packages = ["dblog"],
   author='Alex Sherman',
   author_email='asherman1024@gmail.com',
-  install_requires=['flask', 'python-jrpc==1.1.2'],
+  install_requires=['flask', 'python-jrpc==1.1.5'],
   url='https://github.com/alex-sherman/python-dblog')
